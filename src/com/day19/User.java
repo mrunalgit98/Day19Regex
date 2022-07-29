22762667423fb5a3c8c1dca49eb60b4c3b44bc53
mrunal <mrunalbhoir100@gmail.com>
2022-07-29 11:42:22 +0530

    UC9 Clear all email

--- src/com/day19/User.java
+++ src/com/day19/User.java
@@ -5,144 +5,101 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class User {
+    public static void MultipleEmail() {
+        String email1 = "abc@yahoo.com";
+        String email2 = "abc-100@yahoo.com";
+        String email3 = "abc.100@yahoo.com";
+        String email4 = "abc111@abc.com";
+        String email5 = "abc-100@abc.net";
+        String email6 = "abc.100@abc.com.au";
+        String email7 = "abc@1.com";
+        String email8 = "abc+100@gmail.com";
+        String email9 = "abc@gmail.com.com";
 
-    public static void firstname(){
-      String firstname="[A-Z]{1}[a-z]{2}";
-        Scanner sc =new Scanner(System.in);
-        System.out.println("enter first name");
-        String firstname1=sc.next();
+        String multipleEmail = "[a-zA-Z0-9]+[0-9a-zA-Z]*@[a-z0-9]+[.][a-z]{2,4}[a-z]";
+        Pattern pattern1 = Pattern.compile(multipleEmail);
+        Matcher matcher1 = pattern1.matcher(email1);
+        boolean result1 = matcher1.matches();
 
-        Pattern pattern =Pattern.compile(firstname);
-        Matcher matcher =pattern.matcher(firstname1);
-        boolean result=matcher.matches();
+        if (result1 == true)
+            System.out.println("email 1 is valid");
+        else System.out.println("email 1 is invalid");
 
-        if(result==true)
-            System.out.println("first name is valid");
-        else System.out.println("first name is not valid");
-    }
+        Pattern pattern2 = Pattern.compile(multipleEmail);
+        Matcher matcher2 = pattern2.matcher(email1);
+        boolean result2 = matcher2.matches();
 
-    public static void lastname(){
-        String lastname="[A-Z]{1}[a-z]{2}";
-        Scanner sc =new Scanner(System.in);
-        System.out.println("enter last name");
-        String lastname1=sc.next();
-
-        Pattern pattern =Pattern.compile(lastname);
-        Matcher matcher =pattern.matcher(lastname1);
-        boolean result=matcher.matches();
-
-        if(result==true)
-            System.out.println("last name is valid");
-        else System.out.println("last name is not valid");
-    }
-
-    public static void Email(){
-        String email="[a-zA-Z0-9.]+[0-9a-zA-Z]*@[a-z0-9]+[.][a-z]{2,4}[.][a-z]*";
-        System.out.println("enter email");
-        Scanner sc3 =new Scanner(System.in);
-        String email1=sc3.next();
-
-        Pattern pattern2=Pattern.compile(email);
-        Matcher matcher2=pattern2.matcher(email1);
-        boolean result2=matcher2.matches();
-
-        if(result2== true){
-            System.out.println("Email is valid");
-        }else{
-            System.out.println("Email is not valid");
-        }
-
-    }
-    public static void MobileNumber(){
-        Scanner sc=new Scanner(System.in);
-        String mobileno="(91-)[0-9]{10}";
-        System.out.println("enter phone number");
-        String phno= sc.next();
-
-        Pattern pattern=Pattern.compile(mobileno);
-        Matcher matcher=pattern.matcher(phno);
-        boolean result=matcher.matches();
-
-        if(result==true){
-            System.out.println("mobile number is valid");
-        }else {
-            System.out.println("mobile number is invalid");
-        }
-    }
-
-    public static void passwordValidation(){
-        Scanner sc=new Scanner(System.in);
-        String Password="[a-zA-Z0-9!@#$]{8}";
-        System.out.println("enter password");
-        String password=sc.next();
-
-        Pattern pattern=Pattern.compile(Password);
-        Matcher matcher=pattern.matcher(password);
-        boolean result =matcher.matches();
-        if(result==true){
-            System.out.println("password is valid");
-        }else{
-            System.out.println("password is invalid nter min 8 character");
-        }
-    }
+        if (result1 == true)
+            System.out.println("email 2 is valid");
+        else System.out.println("email 2 is invalid");
 
 
-    public static void passwordValidation2(){
-        Scanner sc=new Scanner(System.in);
-        String Password="[A-Z]+[a-zA-Z0-9!@#$%]{7}";
-        System.out.println("enter password with min one character should be capital");
-        String password=sc.next();
+        Pattern pattern3 = Pattern.compile(multipleEmail);
+        Matcher matcher3 = pattern3.matcher(email1);
+        boolean result3 = matcher3.matches();
 
-        Pattern pattern=Pattern.compile(Password);
-        Matcher matcher=pattern.matcher(password);
-        boolean result =matcher.matches();
-        if(result==true){
-            System.out.println("password is valid");
-        }else{
-            System.out.println("password is invalid enter min 8 character");
-        }
-    }
+        if (result1 == true)
+            System.out.println("email 3 is valid");
+        else System.out.println("email 3 is invalid");
 
 
-    public static void passwordValidation3(){
-        Scanner sc=new Scanner(System.in);
-        String Password="[A-Z]+[0-9][a-zA-Z0-9!@#$%]{6}";
-        System.out.println("enter password with min one number and one Uppercase ");
-        String password=sc.next();
+        Pattern pattern4 = Pattern.compile(multipleEmail);
+        Matcher matcher4 = pattern4.matcher(email1);
+        boolean result4 = matcher4.matches();
 
-        Pattern pattern=Pattern.compile(Password);
-        Matcher matcher=pattern.matcher(password);
-        boolean result =matcher.matches();
-        if(result==true){
-            System.out.println("password is valid");
-        }else{
-            System.out.println("password is invalid enter min 8 character");
-        }
-    }
+        if (result1 == true)
+            System.out.println("email 4 is valid");
+        else System.out.println("email 4 is invalid");
 
-    public static void passwordValidation4(){
-        Scanner sc=new Scanner(System.in);
-        String Password="[A-Z]+[0-9]+[~!@#$%^&*]{1}[a-zA-Z0-9!@#$%]{5}";
-        System.out.println("enter password with min one number and one Uppercase And at least one special character ");
-        String password=sc.next();
 
-        Pattern pattern=Pattern.compile(Password);
-        Matcher matcher=pattern.matcher(password);
-        boolean result =matcher.matches();
-        if(result==true){
-            System.out.println("password is valid");
-        }else{
-            System.out.println("password is invalid enter min 8 character and special charcter");
-        }
+
+        Pattern pattern5 = Pattern.compile(multipleEmail);
+        Matcher matcher5 = pattern5.matcher(email1);
+        boolean result5 = matcher5.matches();
+
+        if (result1 == true)
+            System.out.println("email 5 is valid");
+        else System.out.println("email 5 is invalid");
+
+
+
+        Pattern pattern6 = Pattern.compile(multipleEmail);
+        Matcher matcher6 = pattern6.matcher(email1);
+        boolean result6= matcher6.matches();
+
+        if (result1 == true)
+            System.out.println("email 6 is valid");
+        else System.out.println("email 6 is invalid");
+
+
+        Pattern pattern7 = Pattern.compile(multipleEmail);
+        Matcher matcher7 = pattern7.matcher(email1);
+        boolean result7 = matcher7.matches();
+
+        if (result1 == true)
+            System.out.println("email 7 is valid");
+        else System.out.println("email 7 is invalid");
+
+
+        Pattern pattern8 = Pattern.compile(multipleEmail);
+        Matcher matcher8 = pattern8.matcher(email1);
+        boolean result8 = matcher8.matches();
+
+        if (result1 == true)
+            System.out.println("email 8 is valid");
+        else System.out.println("email 8 is invalid");
+
+
+        Pattern pattern9 = Pattern.compile(multipleEmail);
+        Matcher matcher9= pattern9.matcher(email1);
+        boolean result9= matcher9.matches();
+
+        if (result1 == true)
+            System.out.println("email 9 is valid");
+        else System.out.println("email 9 is invalid");
+
     }
     public static void main(String[] args) {
-//        firstname();
-//        lastname();
-//        Email();
-//        MobileNumber();
-//        passwordValidation();
-  //      passwordValidation2();
-  //      passwordValidation3();
-        passwordValidation4();
+   MultipleEmail();
     }
 }
